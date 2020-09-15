73a11fe724bd237a8924e1d96dcca45e53c83f2f
Aaron Sinnott <aaronsnig@gmail.com>
2020-09-15 17:25:02 -0300

    Add accounts app with rough form design

--- learnr/settings.py
+++ learnr/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "tailwind",
     "theme",
     "marketing",
+    "accounts",
 ]
 
 MIDDLEWARE = [
--- learnr/urls.py
+++ learnr/urls.py
@@ -16,4 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("marketing.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("marketing.urls")),
+    path("accounts/", include("accounts.urls")),
+]
