791c4a65f499689ecb41c87586298ead91197bf9
Aaron Sinnott <aaronsnig@gmail.com>
2020-09-18 11:57:14 -0300

    Initial groundwork for user accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("login", views.login), path("register", views.register)]
+urlpatterns = [
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
+    if request.method == "POST":
+        print(request.POST)
     return render(request, "login.html")
 
 
 def register(request):
+    if request.method == "POST":
+        print(request.POST)
     return render(request, "register.html")
