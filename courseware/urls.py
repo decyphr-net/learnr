c3a5602160cb3d6ff0e8d906475a5ec94229923d
Aaron Sinnott <aaronsnig@gmail.com>
2020-09-21 10:54:36 -0300

    Add name to course URL

--- courseware/urls.py
+++ courseware/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.modules),
+    path("", views.modules, name="course"),
     path("<int:module_id>/", views.lessons),
     path("lesson/<int:lesson_id>", views.units),
     path("unit/<int:unit_id>", views.unit),
