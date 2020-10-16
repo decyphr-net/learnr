8e9fd7120446e5f355805276d53098848477a9ae
Aaron Sinnott <aaronsnig@gmail.com>
2020-10-16 16:37:48 -0300

    Progress

--- progress/models.py
+++ progress/models.py
@@ -1,12 +1,35 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Q
+from django.db.models.constraints import UniqueConstraint
 from django.contrib.auth.models import User
 from courseware.models import Unit
+
+PASSED = [("y", "Yes"), ("n", "No"), ("na", "N/A")]
 
 
 class Progress(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    passed = models.CharField(choices=PASSED, max_length=3)
+
+    def validate_unique(self, *args, **kwargs):
+        print("hello?")
+        super(Progress, self).validate_unique(*args, **kwargs)
+        query = Progress.objects.filter(user=self.user, unit=self.unit)
+        if query.exists():
+            raise ValidationError(
+                {
+                    "unique": [
+                        "Already exists",
+                    ]
+                }
+            )
 
     def __str__(self):
         return f"{self.user.username} - {self.unit.title}"
+
+    def save(self, *args, **kwargs):
+        self.validate_unique()
+        super(Progress, self).save(*args, **kwargs)
