c3f2b70de587fa811c59b9851de87bdb7fee391d
Aaron Sinnott <aaronsnig@gmail.com>
2020-11-05 19:32:12 -0300

    Add Conversation model

--- chat/admin.py
+++ chat/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, Conversation
 
 admin.site.register(Room)
+admin.site.register(Conversation)
--- chat/models.py
+++ chat/models.py
@@ -1,13 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Room(models.Model):
-    """"""
+    """Chat Room
+
+    The set of chat rooms that a user can access
+    """
 
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     slug = models.CharField(max_length=50)
 
     def __str__(self):
-        """"""
+        """Room name"""
         return self.name
+
+
+class Conversation(models.Model):
+    """Converation Model"""
+
+    sender = models.ForeignKey(User, on_delete=models.RESTRICT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    message = models.CharField(max_length=100)
+    room = models.ForeignKey(Room, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return f"{self.sender} - {self.message}"
